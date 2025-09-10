d6ab3eb023994bec8c312d8b6a7aeea9f1b8b1bd
Victor Anderssén <victor.anderssen@gmail.com>
2025-09-10 20:02:32 +0300

    Refactored into functions and added a render texture for the FFT that makes the axis lables visible

--- build.sh
+++ build.sh
@@ -6,15 +6,15 @@ set -xe
 mkdir -p ./build
 
 CC=clang
-CFLAGS="-std=c99 -O3 -Wall -Wextra -Werror"
-LDLIBS="-lm -lraylib -lfftw3"
+CFLAGS="-std=c99 -O3 -march=native -Wall -Wextra -Werror"
+LDLIBS="-lm -lraylib -lfftw3 -lpthread"
 
 SRC_FILES=(
   "./src/main.c"
 )
 INCLUDE_DIRS="-I./include"
 OUTPUT_DIR="./build"
-EXECUTABLE_NAME="main"
+EXECUTABLE_NAME="c_fft_visualizer"
 
 cp -r assets $OUTPUT_DIR
 
